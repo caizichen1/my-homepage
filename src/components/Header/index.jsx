b9005342c53708c8a78c248411113db27c813a03
careston <caizichen@xdfzx.com>
2020-10-12 20:19:08 +0800

    feat:顶部导航条初步开发

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,8 +3,14 @@ import "./index.less"
 
 export default function Header() {
   return (
-    <div className="header">
-      <h2>顶部导航条</h2>
+    <div name="home" className="header-nav">
+      <div className="header-nav-logo">logo</div>
+      <div className="header-nav-label">
+        <a href="#milepost" className="header-nav-link">里程碑</a>
+        <a href="#works" className="header-nav-link">作品</a>
+        <a href="#about" className="header-nav-link">关于</a>
+        <a href="#home" className="header-nav-link">首页</a>
+      </div>
     </div>
   );
 }
