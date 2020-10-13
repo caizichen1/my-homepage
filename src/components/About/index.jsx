54ca117f7df3052ec13f5cc24ac115ee38c82667
careston <caizichen@xdfzx.com>
2020-10-13 17:58:29 +0800

    feat:关于初步设计

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from 'react';
+import "./index.less"
 
 export default function About() {
+  const [aboutLabel, setAboutLabel] = useState([
+    {
+      id: 1,
+      label: '标签1',
+    },
+    {
+      id: 2,
+      label: '长一点的标签2',
+    }
+  ]);
+  const [aboutContent, setAboutContent] = useState('预设数据<br/>预设数据');
+
   return (
-    <div>
-      <h2>关于</h2>
+    <div className="about">
+      <div className="about-title">
+        关于我
+      </div>
+      <div className="about-label">
+        {aboutLabel.map(item => {
+          return <div className="about-label-item" key={item.id}>{item.label}</div>
+        })}
+      </div>
+      <div className="about-content" dangerouslySetInnerHTML={{ __html: aboutContent }} />
     </div>
   );
 }
