fabfdb878e18c6d314fa2d232f93d71a7c6a0174
careston <caizichen@xdfzx.com>
2020-10-14 16:34:08 +0800

    feat:里程碑初步设计

--- src/components/Milepost/index.jsx
+++ src/components/Milepost/index.jsx
@@ -1,9 +1,70 @@
 import React, { useState } from 'react';
+import "./index.less"
 
 export default function Milepost() {
+  const [milepost, setMilepost] = useState([
+    {
+      id: 1,
+      date: '2020.10.12',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 2,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 3,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 4,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 5,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 6,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+    {
+      id: 7,
+      date: '2020.10.13',
+      content: '虚拟滚动测试',
+    },
+  ]);
+
   return (
-    <div>
-      <h2>里程碑</h2>
+    <div className="milepost">
+      <div className="milepost-title">
+        里程碑
+      </div>
+      {/* 新增一层用于隐藏滚动条 */}
+      <div className="milepost-show-outer">
+        <div className="milepost-show">
+          {milepost.map(item => {
+            return (
+              <div className="milepost-item" key={item.id}>
+                <div className="milepost-item-date">
+                  {item.date}
+                </div>
+                <div className="milepost-item-content">
+                  {item.content}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        {/* 遮罩层 */}
+        <div className="milepost-show-cover milepost-show-cover-left" />
+        <div className="milepost-show-cover milepost-show-cover-right" />
+      </div>
     </div>
   );
 }
