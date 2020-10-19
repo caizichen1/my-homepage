b9d7d499bb4e4a29baa7e0a94393e51088564a6f
careston <caizichen@xdfzx.com>
2020-10-19 18:07:39 +0800

    feat:新增锚点定位

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import "./index.less"
 
 export default function Header() {
+
+  //锚点定位
+  const scroll = (className) => {
+    const element = document.getElementsByClassName(className)[0];
+    element.scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
+
   return (
     <div name="home" className="header-nav">
       <div className="header-nav-logo">logo</div>
       <div className="header-nav-label">
-        <a href="#milepost" className="header-nav-link">里程碑</a>
-        <a href="#works" className="header-nav-link">作品</a>
-        <a href="#about" className="header-nav-link">关于</a>
-        <a href="#home" className="header-nav-link">首页</a>
+        <span onClick={() => scroll('milepost')} className="header-nav-link">里程碑</span>
+        <span onClick={() => scroll('works')} className="header-nav-link">作品</span>
+        <span onClick={() => scroll('about')} className="header-nav-link">关于</span>
+        <span onClick={() => scroll('home')} className="header-nav-link">首页</span>
       </div>
     </div>
   );
