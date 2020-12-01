80df1f2441208d3660bb8b85dae6dee0825964bc
careston <caizichen@xdfzx.com>
2020-12-01 18:17:45 +0800

    feat:首页增加箭头动效

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import "./index.less"
+import "./index.less";
+import Arrow from '../Arrow/index'
+import { useRef } from 'react';
 
 export default function Home() {
+  const homeRef = useRef(null)
+
+  const scrollScreen = () => { 
+    const scrollTop = homeRef.current.scrollHeight;
+    window.scroll({
+      top: scrollTop,
+      behavior: 'smooth'
+    });
+  }
+
   return (
-    <div className="home">
-      <div className="home-title">
+    <div className="home" ref={homeRef}>
+      <div className="home-title" >
         Do something you enjoy
+      </div>
+      <div className="home-arrow" onClick={scrollScreen}>
+        <Arrow />
       </div>
     </div>
   );
